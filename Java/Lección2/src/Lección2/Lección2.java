954e9a35d2198fe80a288d490116dd228c5ee2d5
lazaroschutt <schuttlazaro@gmail.com>
2024-06-28 19:25:02 -0300

    Carpeta facultad

---
+++ Java/Lección2/src/Lección2/Lección2.java
@@ -0,0 +1,34 @@
+
+package Lección2;
+
+
+public class Lección2 {
+    public static void main(String[] args) {
+//        var condicion = false;
+//        if(condicion){
+//            System.out.println("Condición Verdadera."); // Condicional Simple.
+//        }
+//        else{
+//            System.out.println("Condición Falsa."); // Condicional Doble.
+//        }
+//        
+//        var numero = 4;
+//        var numeroTexto = "Número desconocido";
+//        if(numero == 1){
+//            numeroTexto = "Número uno.";
+//        }
+//        else if(numero == 2){
+//            numeroTexto = "Número dos.";
+//        }
+//        else if(numero == 3){
+//            numeroTexto = "Número tres.";
+//        }
+//        else if(numero == 4){
+//            numeroTexto = "Número cuatro.";
+//        }
+//        else{
+//            numeroTexto = "Número no encontrado.";
+//        }
+//        System.out.println("numeroTexto = " + numeroTexto);
+    }
+}
